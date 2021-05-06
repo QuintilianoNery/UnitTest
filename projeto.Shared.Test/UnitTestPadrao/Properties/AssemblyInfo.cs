c8d961dcdf71509aa2e97c930bea8e7d6930c27e
Quintiliano <quintiliano@tecsystem.com.br>
2021-05-06 19:11:12 -0300

    Criação de testes unitários

---
+++ projeto.Shared.Test/UnitTestPadrao/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPadrao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPadrao")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5656dd81-5e58-4e5b-8445-9f8ade2a7112")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ projeto.Shared.Test/UnitTestPadrao/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using projeto.Shared.Test;
+
+namespace UnitTestPadrao
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Inicializar()
+        {
+            // usuario = new usuario();
+        }
+
+        [TestMethod]
+        public void VerificaIdadeDeRisco_Idade18Anos_ResultadoTrue()
+        {
+            int idade = 18;
+            bool resultado = Helper.verificaIdadeDeRisco(idade);
+            Assert.IsTrue(resultado);
+        }
+
+        [TestMethod]
+        public void VerificaIdadeDeRisco_Idade15Anos_ResultadoFalse()
+        {
+            int idade = 15;
+            bool resultado = Helper.verificaIdadeDeRisco(idade);
+            Assert.IsFalse(resultado);
+        }
+
+        [TestMethod]
+        public void VerificaNomeDeRisco_Quintiliano_True()
+        {
+            string nome = "Quintiliano";
+            bool resultado = Helper.verificaNomeDeRisco(nome);
+            Assert.IsTrue(resultado);
+        }
+
+        [TestMethod]
+        public void VerificaNomeDeRisco_Corona_False()
+        {
+            string nome = "Corona";
+            bool resultado = Helper.verificaNomeDeRisco(nome);
+            Assert.IsFalse(resultado);
+        }
+
+    }
+}
