6a4c5e759d48df3a3d18535ac93d1bdcafdb33f7
Quintiliano <qpnery@paschoalotto.com.br>
2021-10-11 18:58:32 -0300

    Test Asserts

--- 01 - Testes de Unidade/Demo.Tests/Calculadora.cs
+++ 01 - Testes de Unidade/Demo.Tests/Calculadora.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Demo.Tests
+﻿namespace Demo.Tests
 {
     class Calculadora
     {
---
+++ 01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+namespace Demo.Tests
+{
+    class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornaValorSoma()
+        {
+            //Arrange 
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(2, 2);
+
+            //Assert
+            Assert.Equal(expected: 4, actual: resultado);
+        }
+    }
+}
